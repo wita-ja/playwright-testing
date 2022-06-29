840d787087fd0bea72a16ab62626d50ff2d63d96
Vitalij Janukevic <witalij.janukewicz@gmail.com>
2022-06-29 16:50:56 +0300

    refactoring login specs

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,8 +1,9 @@
 import test, { expect } from "@playwright/test";
 import { LoginPage } from "../pageObjects/loginPage/LoginPage";
+import { Credential, credentials } from "../resources/credentials";
 
 let loginPage: LoginPage;
-const credentials = [{ username: 'standard_user', password: 'secret_sauce', isValid: true }, { username: 'secret_sauce', password: 'standard_user', isValid: false }];
+let loginCredentials: Credential[] = credentials;
 
 test.describe('Login', () => {
   test.beforeEach(async ({ page }) => {
@@ -10,12 +11,12 @@ test.describe('Login', () => {
     await loginPage.open('https://www.saucedemo.com/');
   });
 
-  for (const credential of credentials) {
+  for (const credential of loginCredentials) {
     test(`Login with ${credential.isValid ? 'valid' : 'invalid'} credentials`, async () => {
       await loginPage.enterCredentials(credential.username, credential.password);
       await loginPage.submit();
 
-      const expectedValue = credential === credentials[0] ? true : false;
+      const expectedValue = credential.isValid ? true : false;
 
       await loginPage.page.pause();
       expect(await loginPage.pageHeader.isHidden(), 'Login page logo still visible').toBe(expectedValue);
