576fcb1e30f781550aeeb40961118721bafba6ba
Vitalij Janukevic <witalij.janukewicz@gmail.com>
2022-06-29 14:17:47 +0300

    updating test running script

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   //workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', {open: 'never'}], ['list'], ['allure-playwright']],
+  reporter: [['list'], ['allure-playwright']],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
